c524343cd70989d46be6780cc3d1927838969125
Abdel <abdelouahab.bakadiri>
2023-01-12 22:39:34 +0100

    Get the form values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { UserModel } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-pro';
+
+  onSubmitted(user: UserModel) {
+    console.log(user);
+  }
 }
